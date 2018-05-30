952971fc9bc40d25c496eec50d49001fe594f9df
Paul Sorauer <paul@codered.com.au>
2018-05-31 07:07:50 +1000

    Misc changes to include missing files

--- CodeRed-NPS-Widget/src/nps-widget/store/configureStore.ts
+++ CodeRed-NPS-Widget/src/nps-widget/store/configureStore.ts
@@ -4,19 +4,15 @@ import { createStore, applyMiddleware, Store } from 'redux';
 import { ApplicationState, reducers } from './index';
 
 export default function configureStore(
-  history: History,
   initialState: ApplicationState,
 ): Store<ApplicationState> {
   // create the composing function for our middlewares
-  const composeEnhancers = composeWithDevTools({});
+  // const composeEnhancers = composeWithDevTools({});
 
   // We'll create our store with the combined reducers and the initial Redux state that
   // we'll be passing from our entry point.
   return createStore<ApplicationState>(
     reducers,
-    initialState,
-    composeEnhancers(applyMiddleware(
-      routerMiddleware(history),
-    )),
+    initialState
   );
 }
