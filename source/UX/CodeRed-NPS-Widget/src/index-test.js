dc50a62c0e0cd237844b9b9d979177e65a5871d0
Paul Sorauer <paul@codered.com.au>
2018-06-11 15:25:24 +1000

    Revert "Build testining index entry point as Typescript"
    This reverts commit 094d48ed4e167b17bf07fa234ff2964b1ea0c0c8.

---
+++ source/UX/CodeRed-NPS-Widget/src/index-test.js
@@ -0,0 +1,8 @@
+
+//import Cat from './cat.js';
+//import Dog from './dog.js';
+//import Welcome from './welcome.ts';
+//import NpsWidgetConfig from './nps-widget/config/NpsWidgetConfig';
+//import NpsWidgetStylingConfig from './nps-widget/config/NpsWidgetStylingConfig';
+
+//export { Cat, Dog, Welcome, NpsWidgetConfig, NpsWidgetStylingConfig };
--- source/UX/CodeRed-NPS-Widget/src/index-test.ts
+++
@@ -1,5 +0,0 @@
-
-import StyleDefinitionsMapper, { IStyleDefinitionsMapper } from './nps-widget/services/StyleDefinitionsMapper';
-import StylingDispatcher, { IStylingDispatcher } from './nps-widget/services/StylingDispatcher';
-
-
